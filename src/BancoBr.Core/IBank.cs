7f018f780ac0768fa825c49a5db828b6ff5dfb64
Samuel Rocha <samuelro.net@gmail.com>
2023-02-10 14:38:51 -0300

    feat (Bank): Criação da interfaces IBank e primeiro teste de instanciação

---
+++ src/BancoBr.Core/IBank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoBr.Core
+{
+    public interface IBank
+    {
+        /// <summary>
+        /// Each bank has a unique SWIFT code
+        /// </summary>
+        string SwiftCode { get; set; }
+
+        /// <summary>
+        /// Febraban bank code 
+        /// </summary>
+        string BankCode { get; set; }
+
+        /// <summary>
+        /// Bank name
+        /// </summary>
+        string Name { get; set; }
+    }
+}
---
+++ tests/BancoBr.Tests/CreateBankTest.cs
@@ -0,0 +1,17 @@
+﻿using BancoBr.Core.Banks;
+using Xunit;
+
+namespace BancoBr.Tests
+{
+    public class CreateBankTest
+    {
+        [Fact]
+        public void CreateBradescoBank()
+        {
+            var bank = new Bradesco();
+
+            Assert.Equal("BBDEBRSPSPO", bank.SwiftCode);
+            Assert.Equal("237", bank.BankCode);
+        }
+    }
+}
