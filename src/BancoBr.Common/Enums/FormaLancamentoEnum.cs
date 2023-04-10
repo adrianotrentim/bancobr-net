d4fd3782052c605da7b780444652c1b35fe5e24a
Adriano Trentim Augusto <adriano@divsoft.com.br>
2023-04-10 16:03:47 -0300

    Criado descriptions para enums

--- src/BancoBr.Common/Enums/FormaLancamentoEnum.cs
+++ src/BancoBr.Common/Enums/FormaLancamentoEnum.cs
@@ -1,34 +1,64 @@
-﻿namespace BancoBr.Common.Enums
+﻿using System.ComponentModel;
+
+namespace BancoBr.Common.Enums
 {
     public enum FormaLancamentoEnum
     {
+        [Description("Crédito em Conta Corrente - Mesmo Banco")]
         CreditoContaMesmoBanco = 1,
+        [Description("Cheque de Pagamento")]
         ChequePagamento = 2,
+        [Description("DOC / TED")]
         DOC_TED = 3,
+        [Description("Cartão Salário")]
         CartaoSalario = 4, //Somente para tipo de Serviço = 30
+        [Description("Crédito Conta Poupança - Mesmo Banco")]
         CreditoContaPoupancaMesmoBanco = 5,
+        [Description("O.P. Disposição")]
         OPDisposicao = 10,
+        [Description("Trib. Código de Barras")]
         PagamentoTributosCodigoBarra = 11,
+        [Description("DARF Normal")]
         TributoDARFNormal = 16,
+        [Description("GPS")]
         TributoGPS = 17,
+        [Description("DARF Simples")]
         TributoDARFSimples = 18,
+        [Description("IPTU")]
         TributoIPTU = 19,
+        [Description("Pagamento Autenticado")]
         PagamentoAutenticado = 20,
+        [Description("DARJ")]
         TributoDARJ = 21,
+        [Description("GARE SP - ICMS")]
         TributoGARE_SP_ICMS = 22,
+        [Description("GARE SP - DR")]
         TributoGARE_SP_DR = 23,
+        [Description("GARE SP - ITCMD")]
         TributoGARE_SP_ITCMD = 24,
+        [Description("IPVA")]
         TributoIPVA = 25,
+        [Description("Licenciamento de Veículos")]
         TributoLicenciamento = 26,
+        [Description("DPVAT")]
         TributoDPVAT = 27,
+        [Description("Liquidação Título - Mesmo Banco")]
         LiquidacaoProprioBanco = 30,
+        [Description("Pagamento Título - Outro Banco")]
         PagamentoTituloOutroBanco = 31,
+        [Description("Extrato Conta Corrente")]
         ExtratoContaCorrente = 40,
+        [Description("TED Dif. Titularidade")]
         TEDOutraTitularidade = 41,
+        [Description("TED - Mesma Titularidade")]
         TEDMesmaTitularidade = 43,
+        [Description("TED - Conta Investimento")]
         TEDContaInvestimento = 44,
+        [Description("PIX Transferência")]
         PIXTransferencia = 45,
+        [Description("PIX QR Code")]
         PIXQrCode = 47,
+        [Description("Débito Conta Corrente")]
         DebitoContaCorrente = 50
     }
 }
--- src/BancoBr.Common/Enums/TipoServicoEnum.cs
+++ src/BancoBr.Common/Enums/TipoServicoEnum.cs
@@ -1,36 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace BancoBr.Common.Enums
 {
     public enum TipoServicoEnum
     {
+        [Description("Cobrança")]
         Cobranca = 1,
+        [Description("Bloqueto Eletrônico")]
         BloquetoEletronico = 3,
+        [Description("Conciliação Bancária")]
         ConciliacaoBancaria = 4,
+        [Description("Débitos")]
         Debitos = 5,
+        [Description("Custódia de Cheques")]
         CustodiaCheques = 6,
+        [Description("Gestão de Caixa")]
         GestaoCaixa = 7,
-        CoinsultaMargem = 8,
+        [Description("Consulta Margem")]
+        ConsultaMargem = 8,
+        [Description("Averbação de Consignados")]
         AverbacaoConsignacao = 9,
+        [Description("Pagamento de Dividendos")]
         PagamentoDividentos = 10,
+        [Description("Manutenção de Consignação")]
         ManutencaoConsignacao = 11,
+        [Description("Consignação de Parcela")]
         ConsignacaoParcela = 12,
+        [Description("Glosa de Consignação")]
         GlosaConsignacao = 13,
+        [Description("Consulta de Tributos")]
         ConsultaTributos = 14,
+        [Description("Pagamento de Fornecedores")]
         PagamentoFornecedor = 20,
+        [Description("Pagamento de Tributos")]
         Tributos = 22,
+        [Description("Compror")]
         Compror = 25,
+        [Description("Compror Rotativo")]
         ComprorRotativo = 26,
+        [Description("Alegação do Sacado")]
         AlegacaoSacado = 29,
+        [Description("Pagamento de Salários")]
         PagamentoSalarios = 30,
+        [Description("Vendor")]
         Vendor = 40,
+        [Description("Vendor a Termo")]
         VendorTermo = 41,
+        [Description("Pagamento de Sinistros")]
         PagamentoSinitro = 50,
+        [Description("Despesas de Viagens")]
         DespesaViagem = 60,
+        [Description("Pagamento de Representantes / Vendedores")]
         PagamentoRepresentanteVendedor = 80,
+        [Description("Pagamento de Benefícios")]
         PagamentoBeneficio = 90,
+        [Description("Pagamento Diversos")]
         PagamentoDiverso = 98
     }
 }
