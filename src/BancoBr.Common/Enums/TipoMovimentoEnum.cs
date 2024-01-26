690d2b1a8384a8fa924d0d84bc03f7c158c7be1e
Adriano Trentim <adriano@divsoft.com.br>
2024-01-26 10:51:56 -0300

    Correção de Enumeradores

--- src/BancoBr.Common/Enums/TipoMovimentoEnum.cs
+++ src/BancoBr.Common/Enums/TipoMovimentoEnum.cs
@@ -1,14 +1,24 @@
-﻿namespace BancoBr.Common.Enums
+﻿using System.ComponentModel;
+
+namespace BancoBr.Common.Enums
 {
     public enum TipoMovimentoEnum
     {
+        [Description("Inclusão")]
         Inclusao = 0,
+        [Description("Consulta")]
         Consulta = 1,
+        [Description("Suspensão")]
         Suspensao = 2,
+        [Description("Estorno")]
         Estorno = 3, //Somente para Retorno
+        [Description("Reativação")]
         Reativacao = 4,
+        [Description("Alteração")]
         Alteracao = 5,
+        [Description("Liquidação")]
         Liquidacao = 7,
+        [Description("Exclusão")]
         Exclusao = 9
     }
 }
