1063c8e6dc3876d4fcb420ae28f4bdf13bc06104
lucas.goncalves <lucas.goncalves@divsoft.com.br>
2026-05-07 12:54:09 -0300

    - SiCREDI:  - ajuste header lote  posição 223/230 - deverá ser enviado em branco;  - ajuste segmento A  posição 220/224 - deverá ser enviado em branco quando pagamento de salário;  - ajuste segmento B posição 233/240 - deverá ser enviado em branco;

--- src/BancoBr.CNAB/Sicredi/Banco.cs
+++ src/BancoBr.CNAB/Sicredi/Banco.cs
@@ -42,7 +42,23 @@ namespace BancoBr.CNAB.Sicredi
             return segmento;
         }
 
-       
+        internal override RegistroDetalheBase NovoSegmentoB(TipoLancamentoEnum tipoLancamento)
+        {
+            switch (tipoLancamento)
+            {
+                case TipoLancamentoEnum.CreditoContaMesmoBanco:
+                case TipoLancamentoEnum.CreditoContaPoupancaMesmoBanco:
+                case TipoLancamentoEnum.OrdemPagamento:
+                case TipoLancamentoEnum.TEDMesmaTitularidade:
+                case TipoLancamentoEnum.TEDOutraTitularidade:
+                    return new SegmentoB_Transferencia(this);
+                case TipoLancamentoEnum.PIXTransferencia:
+                    return new SegmentoB_PIX(this);
+                default:
+                    throw new Exception("Tipo de lançamento não implementado");
+            }
+        }
+
         internal override RegistroDetalheBase PreencheSegmentoJ(RegistroDetalheBase segmento, Movimento movimento)
         {
             if (movimento.CodigoInstrucao == CodigoInstrucaoMovimentoEnum.InclusaoRegistroDetalheBloqueado) // Se o movimento for bloqueado, deve ser liberado porque a Sicredi não tem a opção 09
@@ -50,6 +66,23 @@ namespace BancoBr.CNAB.Sicredi
 
             return segmento;
         }
-        
+
+        internal override HeaderLoteBase NovoHeaderLote(TipoLancamentoEnum tipoLancamento)
+        {   
+            if (this.ServicoAtual == TipoServicoEnum.PagamentoSalarios)
+                return new HeaderLote(this);
+
+            return base.NovoHeaderLote(tipoLancamento);
+        }
+
+        internal override RegistroDetalheBase NovoSegmentoA(TipoLancamentoEnum tipoLancamento)
+        {
+            if (this.ServicoAtual == TipoServicoEnum.PagamentoSalarios)
+                return new SegmentoA_TransferenciaFolha(this);
+
+            return base.NovoSegmentoA(tipoLancamento);
+        }
+
+
     }
 }
---
+++ src/BancoBr.CNAB/Sicredi/SegmentoA_TransferenciaFolha.cs
@@ -0,0 +1,34 @@
+﻿using BancoBr.Common.Attributes;
+using BancoBr.Common.Enums;
+
+namespace BancoBr.CNAB.Febraban
+{
+    public class SegmentoA_TransferenciaFolha : SegmentoA_Transferencia
+    {
+        public SegmentoA_TransferenciaFolha(Common.Instances.Banco banco)
+            : base(banco)
+        {
+        }
+
+        #region ::. Propriedades Desativadas .::
+
+        [CampoCNAB(true)]        
+        private new FinalidadeTEDEnum CodigoFinalidadeTED { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Utilizando o tipo de serviço Folha de Pagamento deixar este campo em branco. 
+        /// </summary>
+
+        [CampoCNAB(220, 5)]
+        public string CodigoFinalidadeTEDBranco
+        {
+            get
+            {
+                return string.Empty; 
+            }
+        }
+    }
+}
